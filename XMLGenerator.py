16595395378cc03c857fc8fe382edbc501ace7e7
ViktorSjolind <viktor.sjolind@gmail.com>
2018-10-28 09:58:03 +0200

    Add architecture

---
+++ XMLGenerator.py
@@ -0,0 +1,31 @@
+########################
+#Trip data XML generator
+########################
+
+
+#import our own XMLParser
+
+def run():
+    loadXMLFile()
+    for x in range(10):
+        #offset
+        #save
+        
+
+
+#read an existing XML file
+#it is implied that the XMLParser outputs some kind of list of coordinates
+#this function either creates a global list of coordinates or returns
+#the list so that offsetCoordinates() can change the values
+def loadXMLFile():
+
+
+
+#foreach coordinate randomly offset -+ some 0-10m value
+def offsetCoordinates():
+
+
+#save coordinates to new XML file that has the
+#same structure as the files provided by nextfour
+def saveXMLFile():
+    
