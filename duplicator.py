a8dc2d42929cc2b31aa997d7eafb88836d2575c9
jiwester <35505030+jiwester@users.noreply.github.com>
2018-10-29 15:01:36 +0200

    Update duplicator.py
    Longitude and latitude have switched places in the GPX file that is generated. Does it matter?

--- duplicator.py
+++ duplicator.py
@@ -1,11 +1,12 @@
 import xml.etree.ElementTree as ET
 import random
 
-#Have to do something like this to get same format as original idk
-ET.register_namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
-
 tree = ET.parse('simtrip1.gpx')
 root = tree.getroot()
+
+#To get same format as original GPX file
+ET.register_namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+ET.register_namespace("", "http://www.topografix.com/GPX/1/1")
 
 #List of attributes
 lonsim = []
@@ -20,7 +21,8 @@ for trk in root.findall('{http://www.topografix.com/GPX/1/1}trk'):
             lonsim.append (trkpt.attrib["lon"])
             latsim.append (trkpt.attrib["lat"])
             dptsim.append (trkpt.attrib["dpt"])
-           
+            #lonsim and latsim lists full of same coordinates, check for iteration error?
+            
             #trkpt.attrib["dpt"] = "test" # Changes the value of 'dpt' attribute
             #print (trkpt.attrib)
 
@@ -30,22 +32,28 @@ f_latsim = [float (x) for x in latsim]
 f_dptsim = [float (x) for x in dptsim]
 
 
-#offset coordinates
-#implement random variable somehow
-#test if random.uniform works, does the coordinates vary too much?
 
+#Offset coordinates
+#Implement random variable somehow
+#Test if random.uniform works, does the coordinates vary too much?
+
+a = 0
 for y in f_lonsim:
-    y = round ((y + random.uniform(0.000001, 0.000003)),6)
-    #print (y)
-
+    y = round((y + random.uniform(0.000001, 0.000003)),6)
+    f_lonsim[a] = y
+    a = a + 1
+    
+b = 0
 for z in f_latsim:
-    z = round ((z + random.uniform(0.000001, 0.000003)),6)
+    z = round((z + random.uniform(0.000001, 0.000003)),6)
+    f_latsim[b] = z
+    b = b + 1
     #print (z)
 
 #Convert lists back to string format so we can write them to GPX file
-s_lonsim = [str (x) for x in lonsim]
-s_latsim = [str (x) for x in latsim]
-s_dptsim = [str (x) for x in dptsim]
+s_lonsim = [str (x) for x in f_lonsim]
+s_latsim = [str (x) for x in f_latsim]
+s_dptsim = [str (x) for x in f_dptsim]
 
 
 i = 0; #Variable for coordinate offset iteration
@@ -56,6 +64,7 @@ for trk in root.findall('{http://www.topografix.com/GPX/1/1}trk'):
             trkpt.attrib['lon'] = s_lonsim[i] # Changes the value of longitude attribute
             trkpt.attrib['lat'] = s_latsim[i] # Changes the value of longitude attribute
             i = i + 1
-                      
+            
+            
 tree.write('simtrip1.gpx')
 
