5f780906c22774fedf13af24a47a2c976c552dd8
ViktorSjolind <viktor.sjolind@gmail.com>
2018-12-16 19:46:21 +0200

    Add machine learning dataframe averaging work

--- Machine learning/DBSCAN_Viktor.py
+++ Machine learning/DBSCAN_Viktor.py
@@ -3,6 +3,10 @@
 Based upon the official scikit example,
 the goal is to load a .csv file and find clusters
 
+Todo:
+-fix eps and min_sample values
+-confirm a single trip doesn't make a cluster
+
 Created on Sat Dec 15 19:16:24 2018
 
 @author: Viktor
@@ -17,18 +21,35 @@ import pandas as pd
 import numpy as np
 from random import randint
 
-#Map points are just arrays inside arrays
-points = [[60.1915,21.9099],[60.1915,21.9098]]
-def drawPointsOnMap(superMap, points, color, radiusSize):
-    print('Drawing {} points on map'.format(len(points)))
-    for point in points:        
-        folium.CircleMarker((point), radius = int(radiusSize), fill_color = color, color = color).add_to(superMap)
 
-# dataFrame with all coordinates
+def drawPointsOnMap(superMap, points, color, radiusSize):
+    for point in points:        
+        folium.CircleMarker((point), radius = int(radiusSize), fill=True, fill_color = color, color = color, fill_opacity=0.6).add_to(superMap)
+        folium.CircleMarker((point), radius = 2, fill=True, fill_color = "ff000000", color = "ff000000", fill_opacity=1).add_to(superMap)
+    
+# Take the average for each X coordinates    
+def reducePoints(points):
+    points = sorted(points, key=lambda x: x[0])
+    dividor = 9
+    newPoints = []
+    index = 0
+    pointSumX = 0
+    pointSumY = 0
+    for point in points:
+        pointSumX += point[0]
+        pointSumY += point[1]
+        if index%dividor==0:
+            subArray = [pointSumX / dividor, pointSumY / dividor]
+            newPoints.append(subArray)
+            pointSumX = 0
+            pointSumY = 0
+        index+=1
+    return newPoints
+        
+# DataFrame with all coordinates
+# Map points are just arrays inside arrays
 df = pd.read_csv("DataFrame.csv", index_col = False, header=0)
 
-
-# #############################################################################
 # Compute DBSCAN
 db = DBSCAN(eps=0.3, min_samples=10).fit(df.values)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
@@ -39,12 +60,14 @@ labels = db.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
 
+
+
+
 print('Estimated number of clusters: %d' % n_clusters_)
 print('Estimated number of noise points: %d' % n_noise_)
 
 # Plot result
 superMap = folium.Map(location=[60.19265, 21.914834], zoom_start=20, tiles='OpenStreetMap')
-
 unique_labels = set(labels)
 for k in unique_labels:
     if k == -1:
@@ -52,13 +75,14 @@ for k in unique_labels:
         col = [0, 0, 0, 1]
 
     class_member_mask = (labels == k)
-    xy = df.values[class_member_mask & core_samples_mask]
+    xy = df.values[class_member_mask & core_samples_mask]   
+    
     color = '#{:06x}'.format(randint(0, 256**3))
     drawPointsOnMap(superMap, xy, color, 20)
     
-
-
-
-
-
+newPoints = reducePoints(df.values)
+color = '#{:06x}'.format(randint(0, 256**3))
+drawPointsOnMap(superMap, newPoints, color, 20)
+    
 superMap.save('map-viktor.html')
+print("All done")
